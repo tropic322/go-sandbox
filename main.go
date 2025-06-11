c4a28d0d5700137f585e5dddc8aba67b6a9e2f95
Alexnader <336743@mail.ru>
2025-06-11 14:07:36 +0300

    сортировка

--- main.go
+++ main.go
@@ -4,36 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
+	NG "task-1/services/getters"
 )
 
 func main() {
 	reader := bufio.NewScanner(os.Stdin)
-	fmt.Print("Ведите числа: ")
+	fmt.Println("Введите числа: ")
 	reader.Scan()
 	input := reader.Text()
 
-	// Убираем все разделители кроме цифр
-	cleaned := strings.Map(func(r rune) rune {
-		if r == ',' {
-			return ' '
-		}
-		if r >= '0' && r <= '9' || r == ' ' {
-			return r
-		}
-		return -1
-	}, input)
-
-	parts := strings.Fields(cleaned)
-
-	var numbers []int
-	for _, part := range parts {
-		num, err := strconv.Atoi(part)
-		if err == nil {
-			numbers = append(numbers, num)
-		}
-	}
-
-	fmt.Println(numbers)
+	numbers := NG.GetNumbers(input)
+	fmt.Println("Массив, который был вычитан: ", numbers)
+	NG.SortNumbers(numbers)
+	fmt.Println("Отсортированные элементы: ", numbers)
 }
---
+++ services/getters/numbersGetter.go
@@ -0,0 +1,41 @@
+package getters
+
+import (
+	"strconv"
+	"strings"
+)
+
+func GetNumbers(inputString string) []int {
+	// Убираем все разделители кроме цифр
+	cleaned := strings.Map(func(r rune) rune {
+		if r == ',' {
+			return ' '
+		}
+		if r >= '0' && r <= '9' || r == ' ' {
+			return r
+		}
+		return -1
+	}, inputString)
+
+	parts := strings.Fields(cleaned)
+
+	var numbers []int
+	for _, part := range parts {
+		num, err := strconv.Atoi(part)
+		if err == nil {
+			numbers = append(numbers, num)
+		}
+	}
+	return numbers
+}
+
+func SortNumbers(numbers []int) {
+	n := len(numbers)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if numbers[j] > numbers[j+1] {
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+			}
+		}
+	}
+}
